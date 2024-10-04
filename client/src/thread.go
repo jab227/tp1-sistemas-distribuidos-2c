bdee00120db5b451e7588aa6a556195fda77f304
Daniel Lovera <daanilo2497@gmail.com>
2024-10-04 19:46:01 -0300

    feat: add Thread and Runnable to wrap goroutines

---
+++ client/src/thread.go
@@ -0,0 +1,20 @@
+package src
+
+type Thread[T Runnable] struct {
+	runnable T
+	join     chan error
+}
+
+func NewThread[T Runnable](runnable T) *Thread[T] {
+	capacity := 1
+	join := make(chan error, capacity)
+	return &Thread[T]{runnable: runnable, join: join}
+}
+
+func (t *Thread[T]) Run() {
+	go t.runnable.Run(t.join)
+}
+
+func (t *Thread[T]) Join() error {
+	return <-t.join
+}
