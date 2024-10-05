cdf20c3f9bd2eedd5357058202dce9f97085be67
Daniel Lovera <daanilo2497@gmail.com>
2024-10-04 23:08:40 -0300

    feat: add receiver runnable

---
+++ client/src/receiver.go
@@ -0,0 +1,34 @@
+package src
+
+import (
+	"fmt"
+
+	"github.com/jab227/tp1-sistemas-distribuidos-2c/common/communication"
+)
+
+type Receiver struct {
+	clientConfig *ClientConfig
+	protocol     *communication.Protocol
+}
+
+func NewReceiver(clientConfig *ClientConfig, protocol *communication.Protocol) *Receiver {
+	return &Receiver{clientConfig: clientConfig, protocol: protocol}
+}
+
+func (r *Receiver) Run(join chan error) {
+	if err := r.receive(); err != nil {
+		join <- err
+	}
+	join <- nil
+}
+
+func (r *Receiver) receive() error {
+	// TODO: Aplicar logica cuando se obtengan resultados reales si es necesario
+	for {
+		result, err := r.protocol.RecvResultMessage()
+		if err != nil {
+			return err
+		}
+		fmt.Println(string(result.Payload.Payload.Data))
+	}
+}
