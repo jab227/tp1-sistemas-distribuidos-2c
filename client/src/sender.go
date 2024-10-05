a0043c9f3c61f75f89396350c329b0ce2a1a7f68
Daniel Lovera <daanilo2497@gmail.com>
2024-10-04 23:08:17 -0300

    feat: add sender runnable

---
+++ client/src/sender.go
@@ -0,0 +1,57 @@
+package src
+
+import (
+	"github.com/jab227/tp1-sistemas-distribuidos-2c/common/communication"
+	"github.com/jab227/tp1-sistemas-distribuidos-2c/common/communication/message"
+)
+
+type Sender struct {
+	clientConfig *ClientConfig
+	protocol     *communication.Protocol
+}
+
+func NewSender(clientConfig *ClientConfig, protocol *communication.Protocol) *Sender {
+	return &Sender{clientConfig: clientConfig, protocol: protocol}
+}
+
+func (s *Sender) Run(join chan error) {
+	if err := s.send(); err != nil {
+		join <- err
+	}
+	join <- nil
+}
+
+func (s *Sender) send() error {
+	taskQueue := NewBlockingQueue[*message.DataMessageConfig](s.clientConfig.TaskQueueSize)
+
+	reviewsBatch, deleteReviewsBatch, err := NewBatchFile(s.clientConfig.ReviewsBatch, taskQueue)
+	if err != nil {
+		return err
+	}
+	defer deleteReviewsBatch()
+	gamesBatch, deleteGamesBatch, err := NewBatchFile(s.clientConfig.GamesBatch, taskQueue)
+	if err != nil {
+		return err
+	}
+	defer deleteGamesBatch()
+	fileSender := NewFileSender(s.protocol, taskQueue)
+
+	reviewsBatchThread := NewThread(reviewsBatch)
+	gamesBatchThread := NewThread(gamesBatch)
+	fileSenderThread := NewThread(fileSender)
+	gamesBatchThread.Run()
+	reviewsBatchThread.Run()
+	fileSenderThread.Run()
+
+	if err := reviewsBatchThread.Join(); err != nil {
+		return err
+	}
+	if err := gamesBatchThread.Join(); err != nil {
+		return err
+	}
+	taskQueue.Close()
+	if err := fileSenderThread.Join(); err != nil {
+		return err
+	}
+	return nil
+}
