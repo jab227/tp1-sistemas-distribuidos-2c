44b14678bd7bddbfc4b35871a5a1af5dbc624e7b
Juan <jaguirreb@fi.uba.ar>
2024-10-05 11:27:15 -0300

    implemented routing by id

--- internal/middlewares/routing/routing.go
+++ internal/middlewares/routing/routing.go
@@ -4,19 +4,38 @@ import (
 	"fmt"
 
 	"github.com/jab227/tp1-sistemas-distribuidos-2c/internal/middlewares/client"
+	"github.com/jab227/tp1-sistemas-distribuidos-2c/internal/utils"
 )
 
+type RouteSelector interface {
+	Select(key string) int
+}
+
 type Router struct {
-	keys []string
+	tags []string
+	s    RouteSelector
 	m    *client.IOManager
 }
 
-func NewRouter(input client.InputType, keys []string) (Router, error) {
+func NewRouter(input client.InputType, tags []string, selector RouteSelector) (Router, error) {
 	manager := &client.IOManager{}
 	if err := manager.Connect(input, client.DirectPublisher); err != nil {
 		return Router{}, fmt.Errorf("couldn't create router: %w", err)
 	}
 
-	return Router{m: manager, keys: keys}, nil
+	return Router{m: manager, tags: tags, s: selector}, nil
 }
 
+func (r *Router) Read(onRead func([]byte) error) error {
+	delivery := r.m.Read()
+	error := onRead(delivery.Body)
+	// NOTE(Juan): should we acknowledge in case of err?
+	delivery.Ack(false)
+	return error
+}
+
+func (r *Router) Write(p []byte, key string) error {
+	idx := r.s.Select(key)
+	utils.Assert(idx < len(r.tags), "the index should be less that len(r.tags)")
+	return r.m.Write(p, r.tags[idx])
+}
