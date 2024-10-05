f71ecdff263ec132162994bb449f39fa176a2de4
Federico Ramos <federico.ramos@wazuh.com>
2024-10-05 12:31:49 -0300

    Add projection executable and logging
    - Add projection main.go
    - Add logging capabilites
    - Add logging configuration by env

---
+++ internal/logging/logger.go
@@ -0,0 +1,39 @@
+package logging
+
+import (
+	"fmt"
+	"log/slog"
+	"os"
+)
+
+const LoggerLevelEnvVariable = "LOGGER_LEVEL"
+
+var slogLevelMap = map[string]slog.Level{
+	"info":  slog.LevelInfo,
+	"debug": slog.LevelDebug,
+	"warn":  slog.LevelWarn,
+	"error": slog.LevelError,
+}
+
+func InitLogger(level slog.Level) {
+	opts := &slog.HandlerOptions{
+		Level:     level,
+		AddSource: false,
+	}
+	logger := slog.New(slog.NewTextHandler(os.Stderr, opts))
+	slog.SetDefault(logger)
+}
+
+func InitLoggerWithEnv() error {
+	value, ok := os.LookupEnv(LoggerLevelEnvVariable)
+	if !ok {
+		return fmt.Errorf("environment variable %s not found", LoggerLevelEnvVariable)
+	}
+
+	level, ok := slogLevelMap[value]
+	if !ok {
+		return fmt.Errorf("invalid logger level %s", value)
+	}
+
+	InitLogger(level)
+}
