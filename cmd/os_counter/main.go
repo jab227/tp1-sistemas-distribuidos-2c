14cb6e29ee20e7b7aff8ba702e6695bd91b07ff1
Juan <jaguirreb@fi.uba.ar>
2024-10-06 16:24:03 -0300

    query1 done

---
+++ cmd/os_counter/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"context"
+	"github.com/jab227/tp1-sistemas-distribuidos-2c/internal/controllers"
+	"github.com/jab227/tp1-sistemas-distribuidos-2c/internal/logging"
+	"github.com/jab227/tp1-sistemas-distribuidos-2c/internal/utils"
+	"log/slog"
+)
+
+func main() {
+	err := logging.InitLoggerWithEnv()
+	if err != nil {
+		slog.Error("error creating logger", "error", err.Error())
+		return
+	}
+
+	ctx, cancel := context.WithCancel(context.Background())
+	signal := utils.MakeSignalHandler()
+
+	osCounter, err := controllers.NewOSCounter()
+	if err != nil {
+		slog.Error("error creating os counter", "error", err)
+		return
+	}
+
+	slog.Info("os counter started")
+	go func() {
+		err = osCounter.Run(ctx)
+		if err != nil {
+			slog.Error("error running os counter", "error", err.Error())
+			return
+		}
+	}()
+
+	utils.BlockUntilSignal(signal, osCounter.Done(), cancel)
+}
