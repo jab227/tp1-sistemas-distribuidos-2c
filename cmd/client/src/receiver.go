cc54e0103f94505be9331c7a665b3c5011f6f623
Juan <jaguirreb@fi.uba.ar>
2024-10-10 11:11:35 -0300

    client now closes when all reviews are sent

--- cmd/client/src/receiver.go
+++ cmd/client/src/receiver.go
@@ -3,6 +3,7 @@ package src
 import (
 	"fmt"
 	"log/slog"
+	"os"
 	"strings"
 
 	"github.com/jab227/tp1-sistemas-distribuidos-2c/internal/communication"
@@ -26,7 +27,8 @@ func (r *Receiver) Run(join chan error) {
 }
 
 func (r *Receiver) receive() error {
-	for {
+	var received int
+	for received < 5 {
 		result, err := r.protocol.RecvResultMessage()
 		if err != nil {
 			return err
@@ -37,21 +39,47 @@ func (r *Receiver) receive() error {
 			windows := query1[0]
 			mac := query1[1]
 			linux := query1[2]
-			slog.Info(fmt.Sprintf("Query1: Windows %s, Mac %s, Linux %s\n", windows, linux, mac))
+			fmt.Fprintf(os.Stdout, "Query 1:\n")
+			fmt.Fprintf(os.Stdout, "===========\n")
+			fmt.Fprintf(os.Stdout, "windows: %s\n", windows)
+			fmt.Fprintf(os.Stdout, "mac: %s\n", mac)
+			fmt.Fprintf(os.Stdout, "linux: %s\n", linux)
+			received += 1
 		} else if result.Payload.Header.Type == uint8(message.Query2) {
 			query2 := strings.Split(string(result.Payload.Payload.Data), "\n")
-			slog.Info(fmt.Sprintf("Query2: %s\n", query2))
+			fmt.Fprintf(os.Stdout, "Query 2:\n")
+			fmt.Fprintf(os.Stdout, "===========\n")
+			for i, s := range query2 {
+				fmt.Fprintf(os.Stdout, "%d: %s\n", i, s)
+			}
+			received += 1
 		} else if result.Payload.Header.Type == uint8(message.Query3) {
 			query3 := strings.Split(string(result.Payload.Payload.Data), "\n")
-			slog.Info(fmt.Sprintf("Query3: %s\n", query3))
+			fmt.Fprintf(os.Stdout, "Query 3:\n")
+			fmt.Fprintf(os.Stdout, "===========\n")
+			for i, s := range query3 {
+				fmt.Fprintf(os.Stdout, "%d: %s\n", i, s)
+			}
+			received += 1
 		} else if result.Payload.Header.Type == uint8(message.Query4) {
 			query4 := strings.Split(string(result.Payload.Payload.Data), "\n")
-			slog.Info(fmt.Sprintf("Query4: %s\n", query4))
+			fmt.Fprintf(os.Stdout, "Query 4:\n")
+			fmt.Fprintf(os.Stdout, "===========\n")
+			for i, s := range query4 {
+				fmt.Fprintf(os.Stdout, "%d: %s\n", i, s)
+			}
+			received += 1
 		} else if result.Payload.Header.Type == uint8(message.Query5) {
 			query5 := strings.Split(string(result.Payload.Payload.Data), "\n")
-			slog.Info(fmt.Sprintf("Query5: %s\n", query5))
+			fmt.Fprintf(os.Stdout, "Query 5:\n")
+			fmt.Fprintf(os.Stdout, "===========\n")
+			for i, s := range query5 {
+				fmt.Fprintf(os.Stdout, "%d: %s\n", i, s)
+			}
+			received += 1
 		} else {
 			slog.Debug(fmt.Sprintf("Unknown query type: %d\n", result.Payload.Header.Type))
 		}
 	}
+	return nil
 }
