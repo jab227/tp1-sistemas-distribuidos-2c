9621c57496ec39ee74ab6e21c004eab51a73479f
Juan <jaguirreb@fi.uba.ar>
2024-10-23 12:29:07 -0300

    added support for multiple clients os_counter

---
+++ internal/store/store.go
@@ -0,0 +1,25 @@
+package store
+
+type Store[S any] map[uint32]S
+
+func NewStore[S any]() Store[S] {
+	return make(map[uint32]S)
+}
+
+func (c Store[S]) Set(clientID uint32, state S) {
+	c[clientID] = state
+}
+
+func (c Store[S]) Get(clientID uint32) (S, bool) {
+	state, ok := c[clientID]
+	return state, ok
+}
+
+func (c Store[S]) Delete(clientID uint32) {
+	delete(c, clientID)
+}
+
+func (c Store[S]) Contains(clientID uint32) bool {
+	_, ok := c[clientID]
+	return ok
+}
